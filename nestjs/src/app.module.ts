cbb4982e2a269b86e392afc07960523bfafad61d
Kyle Saunders <kyle.saunders1310@gmail.com>
2022-12-01 03:29:30 +0200

    Setup connectivity to Mongo DB.

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import config from './config'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(config.mongo_db_connection_string)],
   controllers: [AppController],
   providers: [AppService],
 })
